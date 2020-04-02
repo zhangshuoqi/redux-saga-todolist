9c093869bedec91e1379703574f142136e47f8a3
zhangsq5 <zhangsq5@lenovo.com>
2020-04-02 22:01:52 +0800

    update

--- todolist/src/reducers/addUserReducer.js
+++ todolist/src/reducers/addUserReducer.js
@@ -1,5 +1,4 @@
 const addUserReducer = ( state = [], action) => {
-  console.log('add user reduer 22222222', action)
   switch(action.type) {
     case 'ADD_INPUT_SUCCESS':
       return [...action.newState]
